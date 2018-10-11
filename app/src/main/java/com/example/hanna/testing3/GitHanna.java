802285a98e0acaf286996112240e2f30889cd1e3
Hanna <hweissbe@g.jct.ac.il>
2018-10-11 20:03:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hanna/testing3/GitHanna.java
@@ -0,0 +1,13 @@
+package com.example.hanna.testing3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitHanna extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hanna);
+    }
+}
